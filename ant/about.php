bcba2fff5c1247b2673e49246ba3cf033f37f795
sven <121642038@qq.com>
2018-11-10 13:39:07 +0800

    修改关于我们文案

--- ant/about.php
+++ ant/about.php
@@ -25,164 +25,161 @@ $page = 'about';
     <link rel="stylesheet" type="text/css" href="./product_files/index-1.css">
     <link rel="stylesheet" type="text/css" href="./product_files/index-2.css">
     <link rel="stylesheet" type="text/css" href="./product_files/featrue.css">
+    <style>
+        .align-right {
+            text-align: right;
+            margin-bottom: -32px;
+            margin-right: 16px;
+        }
 
+        .img-landscape {
+            margin: 0;
+            max-width: 654px;
+        }
+
+        .main-info {
+            /*line-height: 48px;*/
+        }
+
+        .intro {
+            padding-bottom: 40px;
+        }
+
+        .features {
+            padding: 128px 0;
+        }
+
+        .features h4 {
+            font-size: 20px;
+        }
+
+        .blogs {
+            min-height: 625px;
+            padding: 112px 0 152px 0;
+        }
+
+        .card-body .description {
+            font-weight: 300;
+            padding: 0;
+        }
+
+        .blogs {
+            padding: 112px 0 152px 0;
+        }
+
+        .blogs .col-md-6 {
+            margin-top: 50px;
+            padding: 0 32px;
+        }
+
+        .blogs .blog {
+            border-radius: 4px;
+            /*max-width: 100%;*/
+            /*width: 100%;*/
+            display: inline-block;
+            height: 246px;
+            /*color: #868e96;*/
+            text-decoration: none;
+            border: none;
+            box-shadow: 0 2px 8px rgba(0, 0, 0, .09);
+            margin: 0 auto;
+            -webkit-transition: all .5s;
+            -moz-transition: all .5s;
+            transition: all .5s;
+        }
+
+        .blog:hover {
+            text-decoration: none;
+            -webkit-transform: translateY(-12px);
+            -ms-transform: translateY(-12px);
+            transform: translateY(-12px);
+            box-shadow: 0 12px 24px rgba(0, 0, 0, .09);
+        }
+
+        .card-body {
+            padding: 16px 24px;
+        }
+
+        .title {
+            display: block;
+            font-size: 16px;
+            color: rgba(0, 0, 0, .85);
+            margin-bottom: 4px;
+        }
+
+        .author {
+            color: rgba(0, 0, 0, .45);
+        }
+
+        .avatar {
+            border-radius: 10px;
+            margin-right: 8px;
+        }
+
+        @media only screen and (max-width: 768px) {
+            .img-landscape {
+                margin: 64px 0 32px 0;
+                max-width: 100%;
+            }
+
+            .card {
+                margin: 16px auto;
+            }
+
+            .main-info {
+                padding: 0 24px;
+            }
+
+            .header .main-info {
+                padding: 0;
+            }
+        }
+        .join-us{
+            padding-bottom:120px;
+        }
+        .info-items li {
+            line-height: normal;
+            padding: 14px 20px;
+            display: flex;
+            border-radius: 50px;
+        }
+        .info-items li:hover{
+            background-color: #f5f5f5;
+        }
+        .info-items li > .face {
+            display: none;
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+        }
+        .info-items li > .person {
+            flex: 1;
+            margin: 0 0 0 20px;
+        }
+        .info-items li > .person h4{
+            padding: 0 0 10px 0;
+        }
+        .description h2:before,
+        .info-items li > .person h4:before{
+            display: none;
+        }
+    </style>
 </head>
 <body class="template-home">
-<div style="display: none"><img src="./about_files/logo-with-text.svg" alt=""> <img
-            src="./about_files/bpKcpwimYnZMTarUxCEd.png" alt=""></div>
 
 <div id="react-content">
     <div data-reactroot="" class="page-wrapper">
         <? include 'header.php'?>
 
-        <style>
-            .align-right {
-                text-align: right;
-                margin-bottom: -32px;
-                margin-right: 16px;
-            }
 
-            .img-landscape {
-                margin: 0;
-                max-width: 654px;
-            }
-
-            .main-info {
-                /*line-height: 48px;*/
-            }
-
-            .intro {
-                padding-bottom: 40px;
-            }
-
-            .features {
-                padding: 128px 0;
-            }
-
-            .features h4 {
-                font-size: 20px;
-            }
-
-            .blogs {
-                min-height: 625px;
-                padding: 112px 0 152px 0;
-            }
-
-            .card-body .description {
-                font-weight: 300;
-                padding: 0;
-            }
-
-            .blogs {
-                padding: 112px 0 152px 0;
-            }
-
-            .blogs .col-md-6 {
-                margin-top: 50px;
-                padding: 0 32px;
-            }
-
-            .blogs .blog {
-                border-radius: 4px;
-                /*max-width: 100%;*/
-                /*width: 100%;*/
-                display: inline-block;
-                height: 246px;
-                /*color: #868e96;*/
-                text-decoration: none;
-                border: none;
-                box-shadow: 0 2px 8px rgba(0, 0, 0, .09);
-                margin: 0 auto;
-                -webkit-transition: all .5s;
-                -moz-transition: all .5s;
-                transition: all .5s;
-            }
-
-            .blog:hover {
-                text-decoration: none;
-                -webkit-transform: translateY(-12px);
-                -ms-transform: translateY(-12px);
-                transform: translateY(-12px);
-                box-shadow: 0 12px 24px rgba(0, 0, 0, .09);
-            }
-
-            .card-body {
-                padding: 16px 24px;
-            }
-
-            .title {
-                display: block;
-                font-size: 16px;
-                color: rgba(0, 0, 0, .85);
-                margin-bottom: 4px;
-            }
-
-            .author {
-                color: rgba(0, 0, 0, .45);
-            }
-
-            .avatar {
-                border-radius: 10px;
-                margin-right: 8px;
-            }
-
-            @media only screen and (max-width: 768px) {
-                .img-landscape {
-                    margin: 64px 0 32px 0;
-                    max-width: 100%;
-                }
-
-                .card {
-                    margin: 16px auto;
-                }
-
-                .main-info {
-                    padding: 0 24px;
-                }
-
-                .header .main-info {
-                    padding: 0;
-                }
-            }
-            .join-us{
-                padding-bottom:120px;
-            }
-            .info-items li {
-                line-height: normal;
-                padding: 14px 20px;
-                display: flex;
-                border-radius: 50px;
-            }
-            .info-items li:hover{
-                background-color: #f5f5f5;
-            }
-            .info-items li > .face {
-                display: none;
-                width: 100px;
-                height: 100px;
-                border-radius: 50%;
-            }
-            .info-items li > .person {
-                flex: 1;
-                margin: 0 0 0 20px;
-            }
-            .info-items li > .person h4{
-                padding: 0 0 10px 0;
-            }
-            .description h2:before,
-            .info-items li > .person h4:before{
-                display: none;
-            }
-        </style>
         <section class="intro">
             <div class="container">
                 <div class="header row">
                     <div class="col-md-5">
                         <h1>关于我们</h1>
                         <p class="main-info">
-                            高效的软件研发流程是一个复杂体系，建立这套体系，需要专业的先进的管理思维和管理工具。我们希望基于强大数据、技术、设计的力量，
-                            为领域提供更强大的项目数据可视化能力、产品及行业解决方案。
-                            MasterLab 基因于开源和分享互联网精神，充分利用来自开源的其他力量；全面拥抱开源文化，积极融入开源社区，发扬开源协作创新精神，合理应用开源技术成果为互联网企业服务！</p>
+
+                            MasterLab 发扬开源和分享互联网精神，充分利用来自开源的其他力量，积极融入开源社区，发扬开源协作创新精神，合理应用开源技术为互联网企业服务！</p>
                         <br>
                         <p class="main-info">才启程，不停步，愿与君同行。</p>
                     </div>
@@ -283,7 +280,7 @@ $page = 'about';
             <div class="info-content">
                 <ul  class="text-left info-items" >
                     <li>单元测试  及时反馈极大降低回归测试成本极大减少调试时间部署起来信心十足</li>
-                    <li>持续集成  提早集成，不烦集成，自动化部署
+                    <li>持续集成  提早集成，自动化部署
                     </li>
                     <li>重构  收放自如 偿还技术债务</li>
                     <li>代码规范  用代码进行沟通，清晰表达意图 动态评估取舍 保持简单</li>
