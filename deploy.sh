a4f1865b04a086768dfb9ba40e9e5f5206040f6b
sven <121642038@qq.com>
2018-11-18 10:07:55 +0800

    update .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-set -o errexit -o nounset
-if [ "$TRAVIS_BRANCH" != "master" ]
-then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-  exit 0
-fi
-
-rev=$(git rev-parse --short HEAD)
-
 cd /data/www/masterlab_site/
 
 git config user.name "sven"
