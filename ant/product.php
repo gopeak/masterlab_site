b5e8dd7d0e357589d009772a20293902e4a9bb05
sven <121642038@qq.com>
2019-07-24 23:01:13 +0800

    添加seo文件

--- ant/product.php
+++ ant/product.php
@@ -5,7 +5,7 @@ $page = 'product';
 <!-- saved from url=(0034)https://mobile.ant.design/index-cn -->
 <html class="zh-cn">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <? include 'meta.php' ?>
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no" />
