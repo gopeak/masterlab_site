9c4f654a57a9d103153cbeada2e0778d8373ea30
sven <121642038@qq.com>
2020-03-05 18:32:30 +0800

    更新在线更新的接口

--- ant/upgrade.php
+++ ant/upgrade.php
@@ -13,25 +13,26 @@ $patches = [
     '2.0.2-2.1'=>['patch_url'=>'http://download.888zb.com/2.0.2-2.1.patch.zip','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v2.1'],
     '2.1-3.0'=>['patch_url'=>'http://download.888zb.com/2.1-3.0.patch.zip','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v3.0'],
 ];
+
 /*
-$_GET['action'] = 'getPatchInfo';
+$_GET['action'] = 'get_available_versions';
 $_GET['current_version'] = '2.0';
 $_GET['patch_version'] = '2.0.1';
-http://www.masterlab.vip/upgrade.php?action=getAvailableVersions&current_version=2.0
-http://www.masterlab.vip/upgrade.php?action=getPatchInfo&current_version=2.0&patch_version=2.0.1
+http://www.masterlab.vip/upgrade.php?action=get_available_versions&current_version=2.0
+http://www.masterlab.vip/upgrade.php?action=get_patch_info&current_version=2.0&patch_version=2.0.1
 */
 $action = isset($_GET['action']) ? trim($_GET['action']) : null;
 if(!$action){
     echo json_encode([]);
 }
-if($action=='getAvailableVersions'){
-    getAvailableVersions();
-}
-if($action=='getPatchInfo'){
-    getPatchInfo();
+if(!function_exists($action)){
+    echo json_encode([]);
+}else{
+    $action();
 }
 
-function getPatchInfo()
+
+function get_patch_info()
 {
     global $patches;
     $currentVersion = isset($_GET['current_version']) ? trim($_GET['current_version']) : '';
@@ -45,7 +46,7 @@ function getPatchInfo()
     die;
 }
 
-function getAvailableVersions()
+function get_available_versions()
 {
     global $versions;
     $currentVersion = isset($_GET['current_version']) ? trim($_GET['current_version']) : null;
