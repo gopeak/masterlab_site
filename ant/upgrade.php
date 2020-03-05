df75ef6f852ca3f18f0fa4ac415feb122944b04a
sven <121642038@qq.com>
2020-03-05 18:25:54 +0800

    更新在线更新的接口

--- ant/upgrade.php
+++ ant/upgrade.php
@@ -8,15 +8,17 @@ $versions = [
 ];
 
 $patches = [
-    '2.0-2.0.1'=>['zip_url'=>'http://download.888zb.com/','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v2.0.1'],
-    '2.0.1-2.0.2'=>['zip_url'=>'http://download.888zb.com/','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v2.0.2'],
-    '2.0.2-2.1'=>['zip_url'=>'http://download.888zb.com/','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v2.1'],
-    '2.1-3.0'=>['zip_url'=>'http://download.888zb.com/','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v3.0'],
+    '2.0-2.0.1'=>['patch_url'=>'http://download.888zb.com/2.0-2.0.1.patch.zip','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v2.0.1'],
+    '2.0.1-2.0.2'=>['patch_url'=>'http://download.888zb.com/2.0.1-2.0.2.patch.zip','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v2.0.2'],
+    '2.0.2-2.1'=>['patch_url'=>'http://download.888zb.com/2.0.2-2.1.patch.zip','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v2.1'],
+    '2.1-3.0'=>['patch_url'=>'http://download.888zb.com/2.1-3.0.patch.zip','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v3.0'],
 ];
 /*
 $_GET['action'] = 'getPatchInfo';
 $_GET['current_version'] = '2.0';
 $_GET['patch_version'] = '2.0.1';
+http://www.masterlab.vip/upgrade.php?action=getAvailableVersions&current_version=2.0
+http://www.masterlab.vip/upgrade.php?action=getPatchInfo&current_version=2.0&patch_version=2.0.1
 */
 $action = isset($_GET['action']) ? trim($_GET['action']) : null;
 if(!$action){
