a67832d088910758a4315b6eed6b92576297769a
sven <121642038@qq.com>
2020-03-05 20:01:05 +0800

    更新在线更新的接口

--- ant/upgrade.php
+++ ant/upgrade.php
@@ -1,66 +1,71 @@
 <?php
 header('Content-Type: application/json;charset=utf-8');
+
+// 第一个元素为最新版本
 $versions = [
-    '3.0',
-    '2.1',
-    '2.0.2',
-    '2.0.1'
+    ['version' => '3.0', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v3.0'],
+    ['version' => '2.1', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.1'],
+    ['version' => '2.0.2', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.0.2'],
+    ['version' => '2.0.1', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.0.1']
 ];
 
 $patches = [
-    '2.0-2.0.1'=>['patch_url'=>'http://download.888zb.com/2.0-2.0.1.patch.zip','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v2.0.1'],
-    '2.0.1-2.0.2'=>['patch_url'=>'http://download.888zb.com/2.0.1-2.0.2.patch.zip','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v2.0.2'],
-    '2.0.2-2.1'=>['patch_url'=>'http://download.888zb.com/2.0.2-2.1.patch.zip','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v2.1'],
-    '2.1-3.0'=>['patch_url'=>'http://download.888zb.com/2.1-3.0.patch.zip','release_url'=>'https://github.com/gopeak/masterlab/releases/tag/v3.0'],
+    '2.0-3.0' => 'http://download.888zb.com/2.0-3.0.patch.zip',
+    '2.0.1-3.0' => 'http://download.888zb.com/2.0.1-3.0.patch.zip',
+    '2.0.2-3.0' => 'http://download.888zb.com/2.0.2-3.0.patch.zip',
+    '2.1-3.0' => 'http://download.888zb.com/2.1-3.0.patch.zip'
 ];
 
 /*
-$_GET['action'] = 'get_available_versions';
-$_GET['current_version'] = '2.0';
-$_GET['patch_version'] = '2.0.1';
-http://www.masterlab.vip/upgrade.php?action=get_available_versions&current_version=2.0
-http://www.masterlab.vip/upgrade.php?action=get_patch_info&current_version=2.0&patch_version=2.0.1
+ * $_GET['action'] = 'get_patch_info';
+ * $_GET['current_version'] = '2.0.2';
+ * http://www.masterlab.vip/upgrade.php?action=get_patch_info&current_version=2.0
 */
 $action = isset($_GET['action']) ? trim($_GET['action']) : null;
-if(!$action){
-    echo json_encode([]);
+if (!$action) {
+    echoFailJson("action参数错误!");
 }
-if(!function_exists($action)){
-    echo json_encode([]);
-}else{
+if (!function_exists($action)) {
+    echoFailJson("action参数错误, 函数不存在!");
+} else {
     $action();
 }
 
-
 function get_patch_info()
 {
-    global $patches;
+    global $patches, $versions;
     $currentVersion = isset($_GET['current_version']) ? trim($_GET['current_version']) : '';
-    $patchVersion = isset($_GET['patch_version']) ? trim($_GET['patch_version']) : '';
-    $key = $currentVersion.'-'.$patchVersion;
-    if(!isset($patches[$key])){
-        echo '{}';
-        die;
+    $lastVersionArr = current($versions);
+    $lastVersion = $lastVersionArr['version'];
+    if($currentVersion==$lastVersion){
+        echoFailJson( '已经是最新版本,无需升级!');
     }
-    echo json_encode($patches[$key]);
+    $key = $currentVersion . '-' . @$lastVersionArr['version'];
+    if (!isset($patches[$key])) {
+        echoFailJson($key.' 不可升级!');
+    }
+    $arr = [];
+    $arr['url'] = $patches[$key];
+    $arr['last_version'] = $lastVersionArr;
+    echoSuccessJson('升级可用', $arr);
+}
+
+function echoFailJson($msg)
+{
+    $arr = [];
+    $arr['ret'] = '0';
+    $arr['msg'] = $msg;
+    $arr['data'] = null;
+    echo json_encode($arr);
     die;
 }
 
-function get_available_versions()
+function echoSuccessJson($msg, $data)
 {
-    global $versions;
-    $currentVersion = isset($_GET['current_version']) ? trim($_GET['current_version']) : null;
-    if(!$currentVersion){
-        echo '[]';
-        die;
-    }
-    $returnVersions = [];
-    foreach($versions as $v){
-        if($v==$currentVersion){
-            break;
-        }
-        $returnVersions[] = $v;
-    }
-    echo json_encode($returnVersions);
+    $arr = [];
+    $arr['ret'] = '0';
+    $arr['msg'] = $msg;
+    $arr['data'] = $data;
+    echo json_encode($arr);
     die;
 }
