c6720d76707470b7427dbed992f5aeff67ec2c55
sven <121642038@qq.com>
2020-03-31 19:03:38 +0800

    增加ip限制

--- ant/upgrade.php
+++ ant/upgrade.php
@@ -1,16 +1,39 @@
 <?php
 header('Content-Type: application/json;charset=utf-8');
 
+$ipArr = [
+    '47.244.62.11',
+    '113.104.244.229'
+];
+if(!checkIpAddr($ipArr)){
+    echoFailJson("IP地址受限!");
+}
+
+$lastReleaseHtml = <<<'EOT'
+<p>v2.1.1版本更新内容：</p>
+<ol>
+<li>修复状态流不能显示新状态的bug</li>
+<li>修复“管理/事项配置/自定义字段”的交互问题</li>
+<li>修复“管理/事项配置/自定义界面”的交互问题</li>
+<li>优化：“事项列表和详情”的自定义字段显示</li>
+<li>优化：删除项目时，增加双重确认，提高严谨性</li>
+<li>优化：在线升级的检测和显示</li>
+</ol>
+<a href="#" onclick="$('#menu-upgrade').click();">进入升级界面</a>
+EOT;
+
+
 // 第一个元素为最新版本
 $versions = [
-    //['version' => '3.0', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v3.0'],
-    ['version' => '2.1', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.1'],
-    ['version' => '2.0.2', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.0.2'],
+    //['version' => '3.0', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v3.0','html'],
+    ['version' => '2.1.1', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.1.1', 'title'=>'2.1.1升级通知', 'release_html'=>$lastReleaseHtml],
+    ['version' => '2.1', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.1', 'title'=>'2.1升级通知了', 'release_html'=>$lastReleaseHtml],
+    ['version' => '2.0.2', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.0.2', 'title'=>'2.0.2升级通知', 'release_html'=>$lastReleaseHtml],
     ['version' => '2.0.1', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.0.1']
 ];
 
 $patches = [
-
+    '2.1-2.1.1' => 'http://www.masterlab.vip/downloads/v2.1-v2.1.1-upgrade.zip',
     '2.0.2-2.1' => 'http://download.888zb.com/v2.0.2-v2.1-upgrade.zip',
     '2.0.1-2.0.2' => 'http://download.888zb.com/v2.0.1-v2.0.2-upgrade.zip',
     '2.0-2.0.1' => 'http://download.888zb.com/v2.0-v2.0.1-upgrade.zip'
@@ -31,6 +54,23 @@ if (!function_exists($action)) {
     $action();
 }
 
+/**
+ * 限制IP地址访问
+ * @param $ipArr
+ * @return bool
+ */
+function checkIpAddr($ipArr)
+{
+    if(empty($ipArr)){
+        return true;
+    }
+    $clientIp = $_SERVER['REMOTE_ADDR'];
+    if(!in_array($clientIp, $ipArr)){
+        return false;
+    }
+    return true;
+
+}
 function get_patch_info()
 {
     global $patches, $versions;
@@ -50,6 +90,9 @@ function get_patch_info()
     echoSuccessJson('升级可用', $arr);
 }
 
+/**
+ * @param $msg
+ */
 function echoFailJson($msg)
 {
     $arr = [];
@@ -60,6 +103,10 @@ function echoFailJson($msg)
     die;
 }
 
+/**
+ * @param $msg
+ * @param $data
+ */
 function echoSuccessJson($msg, $data)
 {
     $arr = [];
