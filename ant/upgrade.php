203954ce6bd8f22d2fb74a62d460c21e56b957b3
sven <121642038@qq.com>
2020-04-28 10:43:43 +0800

    更新补丁包

--- ant/upgrade.php
+++ ant/upgrade.php
@@ -3,21 +3,24 @@ header('Content-Type: application/json;charset=utf-8');
 
 $ipArr = [
     //'47.244.62.11',
-    //'113.104.244.229'
+    //'113.104.244.72',
+    //'113.104.246.75'
 ];
 if(!checkIpAddr($ipArr)){
     echoFailJson("升级不可用，IP地址受限!");
 }
 
 $lastReleaseHtml = <<<'EOT'
-<p>v2.1.1版本更新内容：</p>
+<p>v2.1.2版本更新内容：</p>
 <ol>
-<li>修复状态流不能显示新状态的bug</li>
-<li>修复“管理/事项配置/自定义字段”的交互问题</li>
-<li>修复“管理/事项配置/自定义界面”的交互问题</li>
-<li>优化：“事项列表和详情”的自定义字段显示</li>
-<li>优化：删除项目时，增加双重确认，提高严谨性</li>
-<li>优化：在线升级的检测和显示</li>
+<li>在使用宝塔(bt.cn)环境下可一键部署</li>
+<li>优化改进活动日志显示图片的适配问题</li>
+<li>修复bug：协助人头像不能显示的bug</li>
+<li>修复bug：事项标题转义;</li>
+<li>修复bug：事项类型方案无法删除;</li>
+<li>修复bug：事项评论截图上传问题</li>
+<li>移除事项状态关联工作流方案</li>
+<li>不再支持php7.0以下的版本</li>
 </ol>
 <a href="#" onclick="$('#menu-upgrade').click();">进入升级界面</a>
 EOT;
@@ -26,6 +29,7 @@ EOT;
 // 第一个元素为最新版本
 $versions = [
     //['version' => '3.0', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v3.0','html'],
+    ['version' => '2.1.2', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.1.2', 'title'=>'2.1.2升级通知', 'release_html'=>$lastReleaseHtml],
     ['version' => '2.1.1', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.1.1', 'title'=>'2.1.1升级通知', 'release_html'=>$lastReleaseHtml],
     ['version' => '2.1', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.1', 'title'=>'2.1升级通知了', 'release_html'=>$lastReleaseHtml],
     ['version' => '2.0.2', 'release_url' => 'https://github.com/gopeak/masterlab/releases/tag/v2.0.2', 'title'=>'2.0.2升级通知', 'release_html'=>$lastReleaseHtml],
@@ -33,6 +37,8 @@ $versions = [
 ];
 
 $patches = [
+    '2.1-2.1.2'=>'http://download.888zb.com/v2.1-v2.1.2-upgrade.zip',
+    '2.1.1-2.1.2' => 'http://download.888zb.com/v2.1.1-v2.1.2-upgrade.zip',
     '2.1-2.1.1' => 'http://download.888zb.com/v2.1-v2.1.1-upgrade.zip',
     '2.0.2-2.1' => 'http://download.888zb.com/v2.0.2-v2.1-upgrade.zip',
     '2.0.1-2.0.2' => 'http://download.888zb.com/v2.0.1-v2.0.2-upgrade.zip',
