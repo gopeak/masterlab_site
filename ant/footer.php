78d459adae5e151a804b5e6b41a544950f2d2f10
18033262126 <Hzh123321@>
2020-07-03 20:40:46 +0800

    增加工信部链接

--- ant/footer.php
+++ ant/footer.php
@@ -77,7 +77,7 @@
                 <span><a href="#" rel="noopener noreferrer" target="_blank">使用协议</a></span>
             </p>
             <p class="footer-copyright"><!-- react-text: 194 -->Copyright © <!-- /react-text --><span>深圳市敏捷智盛网络有限公司</span>
-                粤ICP备20055300号
+                <a target="_blank" href="http://www.beian.miit.gov.cn">粤ICP备20055300号</a>
             </p>
 
         </div>
